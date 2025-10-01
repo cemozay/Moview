1c824216b07f2a9ad0a99b3b12e68e357abf854d
Yunus Cem Özay <78884331+cemozay@users.noreply.github.com>
2025-10-01 21:15:28 +0300

    Remove sensitive Firebase configuration details from firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -17,17 +17,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB88BenBAVb7xDk4E86yxacobYWOV0vxuU",
-  authDomain: "moview-7.firebaseapp.com",
-  databaseURL:
-    "https://moview-7-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "moview-7",
-  storageBucket: "moview-7.appspot.com",
-  messagingSenderId: "93761073008",
-  appId: "1:93761073008:web:5dc8e9ad90471bbe2929b5",
-  measurementId: "G-MKB8W8Y8ZB",
-};
 
 // Initialize Firebase
 const FirebaseApp = initializeApp(firebaseConfig);
